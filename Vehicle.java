bc167ae6b83db7fd29b67a2ebe8f775a86771f61
modo27 <dokaniam27@gmail.com>
2021-07-18 00:40:00 -0700

    Removed junk

--- Vehicle.java
+++
@@ -1,9 +0,0 @@
-public class Vehicle {
-    private final String registrationNum;
-    private final int driverAge;
-
-    public Vehicle(String registrationNum, int driverAge) {
-        this.registrationNum = registrationNum;
-        this.driverAge = driverAge;
-    }
-}
